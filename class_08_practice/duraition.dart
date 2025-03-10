fbb9081b2ab7d2a5aef5e4bce84c56adbcb94ce8
shahedroks <shahedroks@gmail.com>
2025-03-10 17:46:30 +0600

    toString i con't

--- class_08_practice/duraition.dart
+++ class_08_practice/duraition.dart
@@ -3,22 +3,17 @@ import 'dart:async';
 class Page{
   var countIndex ;
   List image;
-    Page( this.countIndex,this.image);
+    Page( this.countIndex, this.image);
 }
-
 void main()async{
 await  Timer.periodic(Duration(seconds: 3), (Timer time) {
         print("Timer is okk");
         var page = Page(0,[1,2,1,2]);
-        // যদি শেষ ছবিতে না পৌঁছে থাকি, পরবর্তী ছবিতে চলে যাওয়া
         if (page.countIndex <=page.image.length) {
           page.countIndex++;
           print('Duration OK');
         } else {
-          // যদি শেষ ছবিতে পৌঁছে যাই, প্রথম ছবিতে ফিরে যাওয়া
           page.countIndex = 0;
         }}
 );
-
-
 }
